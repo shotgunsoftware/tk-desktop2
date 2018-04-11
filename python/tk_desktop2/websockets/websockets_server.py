48d4f3f3c79ba642f9482ebbb09b0e5aafc4cfef
Josh Tomlinson <josh.tomlinson@autodesk.com>
2018-04-11 11:54:29 -0400

    SG-3021 Multi select for local file linking and desktop2

--- python/tk_desktop2/websockets/websockets_server.py
+++ python/tk_desktop2/websockets/websockets_server.py
@@ -37,8 +37,9 @@ class WebsocketsServer(object):
         # retrieve websockets server from C++
         # TODO: this may be done via standard method in the future.
         manager = QtCore.QCoreApplication.instance().findChild(QtCore.QObject, "sgtk-manager")
-        self._ws_server = manager.findChild(QtWebSockets.QWebSocketServer, "sgtk-web-socket-server")
-        logger.debug("Retrieved websockets server %s" % self._ws_server)
+        server_name = manager.initializeWebSocketServer(True)
+        self._wss_server = QtCore.QCoreApplication.instance().findChild(QtCore.QObject, server_name)
+        logger.debug("Retrieved websockets server %s" % self._wss_server)
 
         # set up certificates handler
         try:
@@ -56,7 +57,8 @@ class WebsocketsServer(object):
         logger.debug("Cert file: %s" % self._sg_certs_handler.cert_path)
         success = self._ws_server.setSslPem(
             self._sg_certs_handler.key_path,
-            self._sg_certs_handler.cert_path
+            self._sg_certs_handler.cert_path,
+            QtNetwork.QSslSocket.VerifyNone
         )
         if not success:
             raise RuntimeError("Could not set up Websockets certificates.")
