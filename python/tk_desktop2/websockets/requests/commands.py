9242c61afed501e62073584963b3a5d228ec196f
Kerby Geffrard <kerby.geffrard@autodesk.com>
2019-09-20 09:42:24 -0400

    Add a set_media_path command to the ws server (#22)
    * Add a set_media_path command to the ws server
    
    JIRA: VMR-2452
    
    DESCRIPTION
    
    Add a set_media_path { "path": "/path/to/media" } command to the websocket
    server so the websocket server can control what is shown in the TaskView's
    player.
    
    * Remove unused import
    
    * Fix typo in docstrings

--- python/tk_desktop2/websockets/requests/commands.py
+++ python/tk_desktop2/websockets/requests/commands.py
@@ -23,6 +23,7 @@ def get_supported_commands():
     from .toolkit_actions import GetActionsWebsocketsRequest
     from .sgc_actions import OpenTaskInSGCreateWebsocketsRequest
     from .sgc_actions import OpenTaskBoardInSGCreateWebsocketsRequest
+    from .sgc_actions import SetMediaPathInSGCreateWebsocketsRequest
     from .list_commands import ListSupportedCommandsWebsocketsRequest
 
     # supported commands
@@ -39,6 +40,7 @@ def get_supported_commands():
         # shotgun Create integration
         "sgc_open_task": {"class": OpenTaskInSGCreateWebsocketsRequest},
         "sgc_open_task_board": {"class": OpenTaskBoardInSGCreateWebsocketsRequest},
+        "set_media_path": {"class": SetMediaPathInSGCreateWebsocketsRequest}
     }
 
     return commands
